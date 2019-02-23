45ee35420b9cb1066160873ae748521046b31023
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-23 13:26:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("548435627746525184")
+let channel =     client.channels.get("548843128552161281")
 setInterval(function() {
 channel.send(`لعيون توماس`);
 }, 30)
